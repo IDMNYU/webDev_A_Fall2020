71ce55768d6037d3c010eb46ffc3e23721d429e8
rebleo <rml444@nyu.edu>
2020-11-23 13:57:02 -0500

    bootstraps

--- week10/code/theMedium.js
+++ week10/code/theMedium.js
@@ -1,56 +1,58 @@
-console.log("the Message!!");
+// console.log("the Message!!");
 
-let theYear =  ["jan", "feb", "march"]
-// console.log(typeof theYear)
-// console.log(typeof myName)
+// let theYear =  ["jan", "feb", "march"]
+// // console.log(typeof theYear)
+// // console.log(typeof myName)
 
-theYear.push("april")
-for (var i = 0; i <theYear.length; i++){
- // console.log(theYear[i]);
-}
-// pass the HTML element into a variable
-// 
-let theBody = document.querySelector('body');
-// console.log(theBody);
-let theButton = document.querySelector("button");
-let theTxt = document.querySelector('h2');
-//
-// triggering an event with a callback function
-theButton.addEventListener('click', thePotatoe);
+// theYear.push("april")
+// for (var i = 0; i <theYear.length; i++){
+//  // console.log(theYear[i]);
+// }
+// // pass the HTML element into a variable
+// // 
+// let theBody = document.querySelector('body');
+// // console.log(theBody);
+// let theButton = document.querySelector("button");
+// let theTxt = document.querySelector('h2');
+// //
+// // triggering an event with a callback function
+// theButton.addEventListener('click', thePotatoe);
 
-function thePotatoe(){
- console.log("cick!!!");
- theBody.style.backgroundColor = 'yellow';
- theBody.style.color = 'green';
- document.querySelector('h2').textContent = "you pressed the button!";
-}
+// function thePotatoe(){
+//  console.log("cick!!!");
+//  theBody.style.backgroundColor = 'yellow';
+//  theBody.style.color = 'green';
+//  document.querySelector('h2').textContent = "you pressed the button!";
+// }
 
-// triggering an event with ES6 arrow notation:
-document.addEventListener('keydown', theEvent => {
- if (theEvent.keyCode === 32){
-  theTxt.textContent = "you pressed space";
- }
-})
+// // triggering an event with ES6 arrow notation:
+// document.addEventListener('keydown', theEvent => {
+//  if (theEvent.keyCode === 32){
+//   theTxt.textContent = "you pressed space";
+//  }
+// })
 
-// common ways of triggering a function you'll often see online
-document.addEventListener('keydown',function() {
- console.log("you pressed any key!")
-})
+// // common ways of triggering a function you'll often see online
+// document.addEventListener('keydown',function() {
+//  console.log("you pressed any key!")
+// })
 
 
 // some p5.js fun....
 // global varialbes are great for browser debugging
-var theCanvas;
-var theCircle;
+
 //
 function setup() {
  console.log("p5!");
- theCanvas = createCanvas(500,500);
- background(0,100,0)
+ createCanvas(windowWidth,windowHeight);
+ background(0,0,0)
 }
 
+function draw(){
+  
+}
 
 function keyPressed() {
   console.log("p5 key pressed!");
-  theCircle = ellipse(random(10,300),100,10,10);
+  ellipse(random(0,windoWidth),random(0,windowHeight),10,10);
 }
